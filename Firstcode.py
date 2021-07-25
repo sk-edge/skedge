4b238b382671e0747fb8cb1f17b73b586a89e5bc
sk-edge <81313325+sk-edge@users.noreply.github.com>
2021-07-25 11:43:54 +0530

    Update Firstcode.py

--- Firstcode.py
+++ Firstcode.py
@@ -1,2 +1,3 @@
 print("Hello World")
-print("This is the first project created for git training")
+print("This is the first project created for git training and now became git master")
+print("Nangalaam apavea apdi")
